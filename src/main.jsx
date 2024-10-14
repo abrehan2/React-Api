30e0dc640b5c19798d3fb5a7eebf1dfabc9d767d
Rehan <rehannajam2@gmail.com>
2024-10-14 14:09:27 +0500

    chore: integrate Tailwind CSS with Flowbite library

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// Imports:
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>,
+)
